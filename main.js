8fd8057d85054659c9ef816659412dcdb44fc227
Fabio Filippini <fipsfabio95@gmail.com>
2022-03-07 15:06:47 +0100

    ok

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const menuOpen = document.querySelector('.header-right a i');
 const menuClose = document.querySelector('.hamburger-menu a i');
 
 
-
 menuOpen.addEventListener('click', function () {
     console.log('menu cliccato');
     hamburgerMenu.style.display = 'block'
